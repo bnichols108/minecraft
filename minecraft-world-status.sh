ddbf16896dc019c7f939d3874f0c67938ec10a1d
Brian <brian.nichols@gmail.com>
2024-01-11 10:15:52 -0500

    Added some more comments for further code changes

--- minecraft-world-status.sh
+++ minecraft-world-status.sh
@@ -13,8 +13,10 @@
 
 
 
-# 1. Check which server is the currently live server. Maybe check a static file that is updated from a different script?
-#currently_live_minecraft_server=`cat /home/brian/maintenance/currently-live-minecraft-server.txt`
+# 1. Check which server is the currently live server. Maybe check a static file that is updated from a different script? 
+# I could also look into hosting this txt file in the cloud, so that if internet did go down on one of the servers, hopefully the other server can update it and each server can be checking it at all times. Just to
+# get more cloud experience. Maybe S3?
+#live_minecraft_server=`cat /home/brian/maintenance/live-minecraft-server.txt`
 
 # 2. Check hostname of current server.
 #echo Hostname is: $HOSTNAME
@@ -30,9 +32,9 @@
 # 5. Check for the bedrock_server running process
 # Adding another portion to check for time because this script is running while my other minecraft backup script is running, which sends a false positive (because the minecraft backup script currently 
 # takes down the minecraft world). 
-
-currenttime=$(date +%H:%M)
-if [[ "$currenttime" > "00:00" && "$currenttime" < "00:10" ]] || [[ "$currenttime" > "06:00" && "$currenttime" < "06:10" ]] || [[ "$currenttime" > "12:00" && "$currenttime" < "12:10" ]] || [[ "$currenttime" > "18:00" && "$currenttime" < "18:10" ]];
+# I should update this later to look for the minecraft backup script instead of specific times, because if the backup script is ran with a different cadence or I run it manually, this will not work.
+current_time=$(date +%H:%M)
+if [[ "$current_time" > "00:00" && "$current_time" < "00:10" ]] || [[ "$current_time" > "06:00" && "$current_time" < "06:10" ]] || [[ "$current_time" > "12:00" && "$current_time" < "12:10" ]] || [[ "$current_time" > "18:00" && "$current_time" < "18:10" ]];
 then
   echo Not running the checks because the minecraft backup script is running. >> /home/brian/maintenance/minecraft-world-status-logs/minecraft-world-status-`date +\%Y-\%m-\%d_\%H-\%M-\%S\%z`.log 2>&1
   exit
