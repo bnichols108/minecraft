1103e60c1adf5a77eb90d3c45e564c761d250d6a
Brian <brian.nichols@gmail.com>
2024-01-27 15:25:04 -0500

    Fixing up the initial build of this script. Grabbing users properly

--- minecraft-world-user-age.sh
+++ minecraft-world-user-age.sh
@@ -7,21 +7,49 @@
 
 # Variables:
 current_users=''
+count=0
 
-# Check and acquire what users are logged in
-screen -S minecraft -X stuff 'list''\015'
-sleep 5
-screen -S minecraft -X hardcopy "/home/brian/maintenance/minecraft-world-user-age.txt"
+while [ $count -le 4 ]
+do
+  screen -S minecraft -X stuff 'list''\015'
+  sleep 3
+  screen -S minecraft -X hardcopy "/home/brian/maintenance/minecraft-world-user-age.txt"
 
-current_users=`tail -n2 /home/brian/maintenance/minecraft-world-user-age.txt | grep -v -e '^$'`
+  current_users=`tail -n3 /home/brian/maintenance/minecraft-world-user-age.txt`
+  echo "$current_users"
+  echo "Current users in while loop is:" "$current_users"
+  echo "========="
+  if [[ `echo "$current_users" | grep "players online"` ]]; then
+    echo "Received the list of users. Exiting while loop."
+    echo "Count currently is: $count"
+    break
+  fi 
+  echo "Count currently is: $count"
+  count=$((count+1))
+  echo "Count currently is: $count"
+done
+
+if [[ $count -eq 5 ]]; then
+  echo "we've ran 5 times, got no players online, exiting"
+  exit 0
+fi
+
+echo "Current users before attempting to remove the first line is: $current_users"
+echo ""
+current_users=`echo "$current_users" | grep -v "players online"`
+
+echo "Current users after attempting to remove the first line is: $current_users"
+echo ""
 
 if [[ -z "$current_users" ]]; then
   echo "There are no users logged in"
+  exit 0
 else
   echo "These are the current_users: $current_users"
   IFS=","
-  for i in $current_users
+  for username in $current_users
   do
-    echo "User: $i"
+    # This echo statement includes the xargs to remove the following usernames that have leading whitspaces.
+    echo "Username: "`echo "$username" | xargs`
   done
 fi
