95c88442182f018ffacbbadf80f6398a17c43103
Brian <brian.nichols@gmail.com>
2024-10-11 02:25:11 -0400

    Changes to backup script to upload to S3

--- minecraft-world-backup.sh
+++ minecraft-world-backup.sh
@@ -11,7 +11,9 @@
 # Variables:
 #minecraft_world_backup_primary_dir=/home/brian/minecraft-backup-primary-drive/minecraft-world-backup/
 #minecraft_world_backup_secondary_dir=/home/brian/minecraft-backup-secondary-drive/minecraft-world-backup/
-
+s3_backup_bucket=s3://minecraft-nighthawks-backup
+s3_backup_limit=56
+archive_name="minecraft-world-backup-$(date '+%Y-%m-%d_%H-%M-%S%z').tar.gz"
 
 # Primary drive backup cleanup
 # Check to ensure we have at least 56 backups, which should equal 14 days worth of backups (14 days x 4 backups per day for every 6 hours).
@@ -64,7 +66,10 @@ sleep 60
 
 # Tar up the entire minecraft directory including the settings, world, etc and timestamp the tar file and place it in the primary backup location.
 echo Creating backup
-tar -zcvf "/home/brian/minecraft-backup-primary-drive/minecraft-world-backup/minecraft-world-backup-$(date '+%Y-%m-%d_%H-%M-%S%z').tar.gz" /home/brian/minecraft
+#tar -zcvf "/home/brian/minecraft-backup-primary-drive/minecraft-world-backup/minecraft-world-backup-$(date '+%Y-%m-%d_%H-%M-%S%z').tar.gz" /home/brian/minecraft
+archive_name
+tar -zcvf "/home/brian/minecraft-backup-primary-drive/minecraft-world-backup/$archive_name" /home/brian/minecraft
+
 
 # Take a copy of the new backup on the primary drive and place it on the secondary drive backup location.
 #echo Copying backup to secondary drive
@@ -73,3 +78,26 @@ tar -zcvf "/home/brian/minecraft-backup-primary-drive/minecraft-world-backup/min
 # Bring up the minecraft world
 echo Messaging screen session to start the minecraft world
 screen -S minecraft-server -X stuff 'LD_LIBRARY_PATH=. /home/brian/minecraft/running/bedrock_server''\015'
+
+# Cleaning up old backups from S3 if applicable
+echo Cleaning up old backups from S3 if applicable
+list_of_s3_backups=( $(aws s3 ls $s3_backup_bucket | awk '{print $4}') )
+
+if [[ ${#list_of_s3_backups[@]} -ge $s3_backup_limit ]]; then
+    echo "There are too many archives. Deleting oldest one."
+    echo Deleting this backup from S3: ${list_of_s3_backups[0]}
+    aws s3 rm $s3_backup_bucket/${list_of_s3_backups[0]}
+else
+    echo There is only ${#list_of_s3_backups[@]} backups, skipping backup cleanup for S3.
+fi
+
+# Uploading latest backup to S3
+echo Uploading latest backup to S3
+s3_upload_status=$(aws s3 cp /home/brian/minecraft-backup-primary-drive/minecraft-world-backup/$archive_name $s3_backup_bucket)
+
+if [[ "$s3_upload_status" =~ "upload:" ]]; then
+    echo S3 file upload successful
+else
+    echo Error occured while uploading archive to S3. Please investigate
+fi
+
